9ad40967752fcfa190fa433c9ce9bedfa43f3ef5
theolemague <60031565+theolemague@users.noreply.github.com>
2020-01-20 22:32:13 +0100

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,12 +1,5 @@
-from multiprocessing import Process, Manager, Array
-import threading
-import queue
 import sysv_ipc
-import sys
 import random
-import time
-import os
-import signal
 
 class Carte:
 	def __init__(self, couleur, nb):
@@ -15,42 +8,49 @@ class Carte:
 	def __str__(self):
 		return self.couleur+" "+str(self.nb)
 
-"""
-Board message queue : 
-1 - To player 1
-2 - To player 2
-3 - From Player
-7 - Endgame
-9 - Send 5 card
-10 - Send key autoryze
-"""
-# Function that check the validity of the card played
-def checkCard(cardstr, stack):
-	cardstr = cardstr.split(" ")
-	card = Carte(cardstr[0], int(cardstr[1]))
-	if card.couleur == stack.couleur :
-		if abs(card.nb - stack.nb) == 1 :
-			return True
-		else : return False
-	else :
-		if card.nb == stack.nb :
-			return True
-		else : return False 
 
-def displayBoard(card, vmq, terminaleMode):
-	if terminaleMode :
-		print(card)
-	else : 
-		m = "3:"+str(card)
-		vmq.send(m.encode(), type = 3)
-
-
-def Board(terminaleMode):
+def Board(developMode):
+	global bmq
+	global vmq
+	global stack
+	global pile
 	bmq = sysv_ipc.MessageQueue(60)
 	vmq = sysv_ipc.MessageQueue(40)
-	
 	stack = []
 	pile = []
+
+
+	"""
+	Board message queue : 
+	1 - To player 1
+	2 - To player 2
+	3 - From Player
+	7 - Endgame
+	9 - Send 5 card
+	10 - Send key autoryze
+	"""
+	# Function that check the validity of the card played
+	def checkCard(cardstr):
+		st = stack[len(stack)-1] 
+		cardstr = cardstr.split(" ")
+		card = Carte(cardstr[0], int(cardstr[1]))
+		if card.couleur == st.couleur :
+			if abs(card.nb - st.nb)==1 or abs(card.nb - st.nb)==9:
+				return True
+			else : return False
+		else :
+			if card.nb == st.nb :
+				return True
+			else : return False 
+
+	def displayBoard( developMode):
+		card = stack[len(stack)-1]
+		if developMode :
+			print(card)
+		else : 
+			m = "3:"+str(card)+":"+str(len(pile))
+			vmq.send(m.encode(), type = 3)
+
 
 	for i in range(1,11):
 		pile.append(Carte("Rouge", i))
@@ -75,8 +75,8 @@ def Board(terminaleMode):
 	bmq.send(msghand1.encode(), type = 9)
 	bmq.send(msghand2.encode(), type = 9)
 	
-	displayBoard(stack[len(stack)-1], vmq, terminaleMode)
 	while True : 
+		displayBoard(developMode)
 		# Waiting for a player message
 		try : 
 			m, t = bmq.receive(type = 3)
@@ -104,12 +104,11 @@ def Board(terminaleMode):
 				bmq.send(empty.encode(), type = 7)		
 				break	
 		else :
-			if checkCard(m[1], stack[len(stack)-1]) :
+			if checkCard(m[1]) :
 				# If the card is valid
 				bmq.send(m[1].encode(), type = int(m[0]))
 				card = m[1].split(" ")
 				stack.append(Carte(card[0], int(card[1])))
-				displayBoard(stack[len(stack)-1], vmq, terminaleMode)
 			else :
 				if len(pile)==0:				
 					empty = ""
